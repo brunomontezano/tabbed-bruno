7b18eddad6e54acb76473f499c7a85e7c7cc7e33
Bruno Montezano <brunodoyt@gmail.com>
2021-07-27 02:12:02 -0300

    Change font.

--- config.def.h
+++ config.def.h
@@ -1,7 +1,7 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const char font[]        = "Fantasque Sans Mono:size=9";
+static const char font[]        = "JetBrains Mono:size=9";
 static const char* normbgcolor  = "#2e3440";
 static const char* normfgcolor  = "#d8dee9";
 static const char* selbgcolor   = "#e0af68";
